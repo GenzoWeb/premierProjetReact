b076b8ad8f9e510f0eb1be6e1ad28eaad7d4ffa6
GenzoWeb <genzo10@hotmail.fr>
2022-09-02 11:30:08 +0200

    mise à jour ajout de projets

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -9,20 +9,22 @@ function Sidebar() {
 
    return (
       <div className="sidebar">
-         <div className="head">
-            <img className="img_sidebar" src="/images/developpeur.jpg" alt="développeur sur son bureau" />
-            <h2>Julien Desmoniere</h2>
-         </div>
-         <div className="header_burger" onClick={() =>{toggleMenu()}}>
-            <Burger menuOpen={menuShow}></Burger>
-         </div>
-         <NavMenu menuOpen={menuShow}></NavMenu>
-         <div className="icons_sidebar">
-            <div className="fa_sidebar">
-               <a href="https://github.com/GenzoWeb"><i className="fab fa-github"></i></a>
-               <a href="https://www.facebook.com/julien.desmoniere"><i className="fab fa-facebook"></i></a>
+         <div className="sidebar_content">
+            <div className="head">
+               <img className="img_sidebar" src="/images/developpeur.jpg" alt="développeur sur son bureau" />
+               <h2>Julien Desmoniere</h2>
             </div>
-            <p>JD</p>
+            <div className="header_burger" onClick={() =>{toggleMenu()}}>
+               <Burger menuOpen={menuShow}></Burger>
+            </div>
+            <NavMenu menuOpen={menuShow}></NavMenu>
+            <div className="icons_sidebar">
+               <div className="fa_sidebar">
+                  <a href="https://github.com/GenzoWeb"><i className="fab fa-github"></i></a>
+                  <a href="https://www.facebook.com/julien.desmoniere"><i className="fab fa-facebook"></i></a>
+               </div>
+               <p>JD</p>
+            </div>
          </div>
       </div>
    );
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,59 +1,33 @@
 export const portfolioData = [
   {
-    id: 1,
-    title: 'Mon premier projet',
-    realisation : 'Réalisé pendant ma formation',
-    languages: ['html','css'],
-    source: "https://www.genzo.fr/projet1/index.html",
-    info: "Mon premier projet durant ma formation. Intégration d'une maquette pour une agence web. Je devais réaliser un site qui devait tenir sur une page avec un menu qui reste visible en haut de page. Différentes sections à réaliser avec un formulaire de contact non fonctionnel. Ce site devait être réalisé sans framework juste HTML/CSS.",
-    picture: './images/p1.png'
+    id: 10,
+    title: 'Agenda professionel',
+    realisation : 'Projet personnel',
+    languages: ['php', 'css'],
+    github: 'https://github.com/GenzoWeb/Agenda',
+    source: "https://www.genzo.fr/agenda/public",
+    info: "Je me crée un agenda pour faire le compte de mes jours de congés et de RTT sur une année de travail ( juin à mai )",
+    picture: './images/p10.png'
   },
   {
-    id: 2,
-    title: "Création d'un site wordpress",
-    realisation : 'Réalisé pendant ma formation',
-    languages: ['wordpress','php', 'css'],
-    source: "https://www.genzo.fr/projet2",
-    info: "Création d'un site wordpress pour l'office de tourisme. A la découverte de wordpress en créant un site pour l'office de tourisme d'une ville. Différentes sections imposées comme les actualités, des activités et une page de contact.",
-    picture: './images/p2.jpg'
-  },
-  {
-    id: 3,
-    title: "Création d'une carte intéractive",
-    realisation : 'Réalisé pendant ma formation',
-    languages: ['javascript','css'],
-    source: "https://www.genzo.fr/projet3",
-    info: "Création d'une carte intéractive de location de vélos à l'aide d'une API de cartographie. Mon premier site en javascript. Je devais réaliser une single page application simulant la réservation de vélos dans une ville. Les vélos dont donc répartis dans de nombreuses stations dans la ville. L'utilisateur a la possibilité de réserver un vélo à condition qu'il reste des vélos disponible. La réservation est alors temporairement enregistrée sur le navigateur du visiteur. Aucun code coté serveur. Les données doivent provenir de l'API en temps réel.",
-    picture: './images/p3.png'
-  },
-  {
-    id: 4,
-    title: 'Mon premier projet en PHP',
-    realisation : 'Réalisé pendant ma formation',
-    languages: ['javascript','php','css'],
-    github: 'https://github.com/GenzoWeb/projet4',
-    source: "https://www.genzo.fr/projet4",
-    info: "Création d'un blog pour un écrivain. Je devais développez une application de blog en PHP sans framework avec base de données MySQL. Cette application doit fournir une interface frontend et une interface backend. Je l'ai réalisé avec une structure MVC, orienté objet, CRUD, commentaires possible sur les billets, signalement des commentaires...",
-    picture: './images/p4.png'
-  },
-  {
-    id: 5,
-    title: 'Projet pour valider ma formation',
-    realisation : 'Réalisé pendant ma formation',
-    languages: ['javascript','css','php'],
-    source: "https://www.genzo.fr/projet5",
-    info: "Projet libre. J'ai fait une sorte de synthèse pour essayer d'utiliser tout languages que j'ai appris pendant la formation. J'ai fait le choix d'utiliser une API de football pour suivre le classement et matchs de certains championnats. La possibilité de s'inscrire pour jouer au jeu des grilles de pronostics... Interface frontend pour les joueurs et backend.",
-    picture: './images/p5.png'
-  },
-  {
-    id: 6,
-    title: 'Mon premier portfolio',
+    id: 9,
+    title: 'Premier projet Angular',
     realisation : 'Projet personnel',
     languages: ['javascript', 'css'],
-    github: 'https://github.com/GenzoWeb/Portfolio',
-    source: "https://www.genzo.fr/projet6",
-    info: "Mon premier portfolio pour lister les projets réalisés. J'ai décidé de me lancer dans un projet sans guide pour le réaliser et continuer à coder.",
-    picture: './images/p6.jpg'
+    github: 'https://github.com/GenzoWeb/premierProjetAngular',
+    source: "https://www.genzo.fr/projet9",
+    info: "Je découvre le framework Angular en créant un site qui clone netflix en utilisant l'API de tmdb pour récupérer les séries et films.",
+    picture: './images/p9.png'
+  },
+  {
+    id: 8,
+    title: 'Mon second portfolio',
+    realisation : 'Projet personnel',
+    languages: ['javascript', 'css'],
+    github: 'https://github.com/GenzoWeb/newPortfolio',
+    source: "https://www.genzo.fr/projet8",
+    info: "Je découvre les librairies javascript via npm. Je décide de refaire un portfolio pour intégrer des librairies comme sass, babel, browser, prefix, swup pour réaliser cette application.",
+    picture: './images/p8.png'
   },
   {
     id: 7,
@@ -66,13 +40,59 @@ export const portfolioData = [
     picture: './images/p7.png'
   },
   {
-    id: 8,
-    title: 'Mon second portfolio',
+    id: 6,
+    title: 'Mon premier portfolio',
     realisation : 'Projet personnel',
     languages: ['javascript', 'css'],
-    github: 'https://github.com/GenzoWeb/newPortfolio',
-    source: "https://www.genzo.fr/projet8",
-    info: "Je découvre les librairies javascript via npm. Je décide de refaire un portfolio pour intégrer des librairies comme sass, babel, browser, prefix, swup pour réaliser cette application.",
-    picture: './images/p8.png'
+    github: 'https://github.com/GenzoWeb/Portfolio',
+    source: "https://www.genzo.fr/projet6",
+    info: "Mon premier portfolio pour lister les projets réalisés. J'ai décidé de me lancer dans un projet sans guide pour le réaliser et continuer à coder.",
+    picture: './images/p6.jpg'
   },
+  {
+    id: 5,
+    title: 'Projet pour valider ma formation',
+    realisation : 'Réalisé pendant ma formation',
+    languages: ['javascript','css','php'],
+    source: "https://www.genzo.fr/projet5",
+    info: "Projet libre. J'ai fait une sorte de synthèse pour essayer d'utiliser tout languages que j'ai appris pendant la formation. J'ai fait le choix d'utiliser une API de football pour suivre le classement et matchs de certains championnats. La possibilité de s'inscrire pour jouer au jeu des grilles de pronostics... Interface frontend pour les joueurs et backend.",
+    picture: './images/p5.png'
+  },
+  {
+    id: 4,
+    title: 'Mon premier projet en PHP',
+    realisation : 'Réalisé pendant ma formation',
+    languages: ['javascript','php','css'],
+    github: 'https://github.com/GenzoWeb/projet4',
+    source: "https://www.genzo.fr/projet4",
+    info: "Création d'un blog pour un écrivain. Je devais développez une application de blog en PHP sans framework avec base de données MySQL. Cette application doit fournir une interface frontend et une interface backend. Je l'ai réalisé avec une structure MVC, orienté objet, CRUD, commentaires possible sur les billets, signalement des commentaires...",
+    picture: './images/p4.png'
+  },
+  {
+    id: 3,
+    title: "Création d'une carte intéractive",
+    realisation : 'Réalisé pendant ma formation',
+    languages: ['javascript','css'],
+    source: "https://www.genzo.fr/projet3",
+    info: "Création d'une carte intéractive de location de vélos à l'aide d'une API de cartographie. Mon premier site en javascript. Je devais réaliser une single page application simulant la réservation de vélos dans une ville. Les vélos dont donc répartis dans de nombreuses stations dans la ville. L'utilisateur a la possibilité de réserver un vélo à condition qu'il reste des vélos disponible. La réservation est alors temporairement enregistrée sur le navigateur du visiteur. Aucun code coté serveur. Les données doivent provenir de l'API en temps réel.",
+    picture: './images/p3.png'
+  },
+  {
+    id: 2,
+    title: "Création d'un site wordpress",
+    realisation : 'Réalisé pendant ma formation',
+    languages: ['wordpress','php', 'css'],
+    source: "https://www.genzo.fr/projet2",
+    info: "Création d'un site wordpress pour l'office de tourisme. A la découverte de wordpress en créant un site pour l'office de tourisme d'une ville. Différentes sections imposées comme les actualités, des activités et une page de contact.",
+    picture: './images/p2.jpg'
+  },
+  {
+    id: 1,
+    title: 'Mon premier projet',
+    realisation : 'Réalisé pendant ma formation',
+    languages: ['html','css'],
+    source: "https://www.genzo.fr/projet1/index.html",
+    info: "Mon premier projet durant ma formation. Intégration d'une maquette pour une agence web. Je devais réaliser un site qui devait tenir sur une page avec un menu qui reste visible en haut de page. Différentes sections à réaliser avec un formulaire de contact non fonctionnel. Ce site devait être réalisé sans framework juste HTML/CSS.",
+    picture: './images/p1.png'
+  }
 ]
